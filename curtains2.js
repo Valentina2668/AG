6e5a9fc718a1f84067d445b2dcf0632cbe532787
Valentina <68551592+Valentina2668@users.noreply.github.com>
2023-07-20 14:34:37 +0300

    ClassWork

--- curtains2.js
+++ curtains2.js
@@ -1,5 +1,31 @@
 $(document).ready(function () {
-    const shiftCurtain = 0.2
+    let curtainOpen = false;
+    const shiftCurtain = 0.2;
+    const animationDuration = 1000;
+    $(".rope").click(function () {
+        if (curtainOpen) {
+            $('.right-curtain').css('transform', 'matrix(1, 0, 0, 1, 0, 0)');
+            $('.left-curtain').css('transform', 'matrix(1, 0, 0, 1, 0, 0)');
+            $('.right-curtain').animate({
+                left: 0
+            }, { duration: animationDuration * 4, queue: false });
+            $('.left-curtain').animate({
+                left: 0
+            }, { duration: animationDuration * 4, queue: false });
+        } else {
+            curtainsMoving('.left-curtain', - 1);
+            curtainsMoving('.right-curtain', 1);
+            setTimeout(() => {
+                $('.left-curtain').animate({
+                    left: -400
+                }, { duration: animationDuration * 5, queue: false });
+                $('.right-curtain').animate({
+                    left: 400
+                }, { duration: animationDuration * 5, queue: false });
+            }, animationDuration);
+        }
+        curtainOpen = !curtainOpen;
+    });
     function curtainsMoving(selector, sight) {
         $(selector).animate(
             { smile: shiftCurtain * sight },
@@ -7,45 +33,40 @@ $(document).ready(function () {
                 step: function (now) {
                     console.log(now)
                     $(selector).css('transform', 'matrix(1.0, 0.0, ' + (now) + ', 1.0, 0.0, 0.0)');
-
                 },
-                duration: 200, queue: false
+                duration: animationDuration, queue: false
             }
         );
         $(selector).animate({
             left: 55 * sight
-        }, { duration: 200, queue: false });
+        }, { duration: animationDuration, queue: false });
     }
-    $(".cutrains").click(function () {
-        curtainsMoving('.left-curtain', -1);
-        curtainsMoving('.right-curtain', 1 );
-        // $('.left-curtain').animate(
-        //     { smile: shiftCurtain * -1 },
-        //     {
-        //         step: function (now) {
-        //             console.log(now)
-        //             $('.left-curtain').css('transform', 'matrix(1.0, 0.0, ' + now + ', 1.0, 0.0, 0.0)');
+    // $('.left-curtain').animate(
+    //     { smile: shiftCurtain * -1 },
+    //     {
+    //         step: function (now) {
+    //             console.log(now)
+    //             $('.left-curtain').css('transform', 'matrix(1.0, 0.0, ' + now + ', 1.0, 0.0, 0.0)');
 
-        //         },
-        //         duration: 200, queue: false 
-        //     }
-        // );
-        // $(".left-curtain").animate({
-        //     left: -55
-        // }, { duration: 200, queue: false });
-        // $('.right-curtain').animate(
-        //     { smile: shiftCurtain },
-        //     {
-        //         step: function (now) {
-        //             console.log(now)
-        //             $('.right-curtain').css('transform', 'matrix(1.0, 0.0, ' + now + ', 1.0, 0.0, 0.0)');
+    //         },
+    //         duration: 200, queue: false 
+    //     }
+    // );
+    // $(".left-curtain").animate({
+    //     left: -55
+    // }, { duration: 200, queue: false });
+    // $('.right-curtain').animate(
+    //     { smile: shiftCurtain },
+    //     {
+    //         step: function (now) {
+    //             console.log(now)
+    //             $('.right-curtain').css('transform', 'matrix(1.0, 0.0, ' + now + ', 1.0, 0.0, 0.0)');
 
-        //         },
-        //         duration: 200, queue: false 
-        //     }
-        // );
-        // $(".right-curtain").animate({
-        //     right: -55
-        // }, { duration: 200, queue: false });
-    });
+    //         },
+    //         duration: 200, queue: false 
+    //     }
+    // );
+    // $(".right-curtain").animate({
+    //     right: -55
+    // }, { duration: 200, queue: false });
 });
